36a14ceebfc703aab2c65130bf47516e3222cb1e
Koki Okada <okada.git5151@gmail.com>
2026-05-29 11:12:31 +0900

    feat: ghosttyとzellij連携

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -1,7 +1,15 @@
 { pkgs, ... }:
+let
+  ghosttyZellij = pkgs.writeShellScript "ghostty-zellij" ''
+    exec ${pkgs.zellij}/bin/zellij attach -c ghostty
+  '';
+in
 {
   programs.ghostty = {
     enable = true;
     package = pkgs.ghostty-bin;
+    settings = {
+      command = "${ghosttyZellij}";
+    };
   };
 }
